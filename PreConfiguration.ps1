0fef0f3f84a5afad9672ee094b3b5ddb58e86808
Eduardo Hamue <eduardo.hamue@aspentech.com>
2024-03-12 17:27:30 +0000

    Add logs

--- PreConfiguration.ps1
+++ PreConfiguration.ps1
@@ -16,17 +16,33 @@
     Copy-Item -Path $sourcePath\* -Destination $destinationPath -Recurse -Force
 }
 
-Copy-WithCreateDirectory -sourcePath "\\hqazrndfs01.corp.aspentech.com\upload$\aspenONEV14.0\ProMV\RC" -destinationPath "C:\p4\Media\"
+Write-Host "Media Copy start"
+
+Copy-WithCreateDirectory -sourcePath "\\hqazrndfs01.corp.aspentech.com\upload$\aspenONEV14.0\ProMV\RC" -destinationPath "C:\p4\Media\"  >> C:\p4\ProMVConfiguration.git\logs\logMedia.txt
 
 
-Start-Process -FilePath 'C:\Program Files\AspenTech\InfoPlus.21\db21\code\IP21Rename.exe' -ArgumentList "-nq" -WorkingDirectory 'C:\Program Files\AspenTech\InfoPlus.21\db21\code\' -Wait
+Write-Host "Changing machine name"
 
-& "C:\Program Files\AspenTech\InfoPlus.21\db21\code\tsk_client.exe" /start_infoplus21
+Start-Process -FilePath 'C:\Program Files\AspenTech\InfoPlus.21\db21\code\IP21Rename.exe' -ArgumentList "-nq" -WorkingDirectory 'C:\Program Files\AspenTech\InfoPlus.21\db21\code\' -Wait >> C:\p4\ProMVConfiguration.git\logs\logRenameMachine.txt
 
-C:\ProgramData\Aspentech\AspenVirtualPlant\runscript.vbs
 
-iisreset
+Write-Host "info plus starting"
+& "C:\Program Files\AspenTech\InfoPlus.21\db21\code\tsk_client.exe" /start_infoplus21 >> C:\p4\ProMVConfiguration.git\logs\logStartInfoPlus.txt
 
-iisreset
 
-C:\ProgramData\Aspentech\AspenVirtualPlant\startVP.bat
+Write-Host "Virtual Plant starting"
+
+C:\ProgramData\Aspentech\AspenVirtualPlant\runscript.vbs >> C:\p4\ProMVConfiguration.git\logs\logVirtualPlant.txt
+
+
+
+Write-Host "StartVP"
+
+C:\ProgramData\Aspentech\AspenVirtualPlant\startVP.bat >> C:\p4\ProMVConfiguration.git\logs\logStartVp.txt
+
+
+Write-Host "ISS restart"
+
+iisreset >> C:\p4\ProMVConfiguration.git\logs\logIssReset.txt
+
+iisreset >> C:\p4\ProMVConfiguration.git\logs\logIssReset.txt
