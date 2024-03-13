50787b699b14284fbb076138e73505507e02a190
Eduardo Hamue <eduardo.hamue@aspentech.com>
2024-03-13 18:43:43 +0000

    Remove copy media

--- PreConfiguration.ps1
+++ PreConfiguration.ps1
@@ -1,27 +1,4 @@
-﻿function Copy-WithCreateDirectory {
-    param (
-        [string]$sourcePath,
-        [string]$destinationPath
-    )
-
-    # Check if the destination directory exists, if not, create it.
-    if (-not (Test-Path -Path $destinationPath -PathType Container)) {
-        New-Item -Path $destinationPath -ItemType Directory -Force
-    }
-
-    # Get the base name of the source directory
-    $sourceBaseName = (Get-Item $sourcePath).BaseName
-
-    # Copy files using Copy-Item to the destination without creating a subfolder
-    Copy-Item -Path $sourcePath\* -Destination $destinationPath -Recurse -Force
-}
-
-Write-Host "Media Copy start"
-
-Copy-WithCreateDirectory -sourcePath "\\hqazrndfs01.corp.aspentech.com\upload$\aspenONEV14.0\ProMV\RC" -destinationPath "C:\p4\Media\"  >> C:\p4\ProMVConfiguration.git\logs\logMedia.txt
-
-
-Write-Host "Changing machine name"
+﻿Write-Host "Changing machine name"
 
 Start-Process -FilePath 'C:\Program Files\AspenTech\InfoPlus.21\db21\code\IP21Rename.exe' -ArgumentList "-nq" -WorkingDirectory 'C:\Program Files\AspenTech\InfoPlus.21\db21\code\' -Wait >> C:\p4\ProMVConfiguration.git\logs\logRenameMachine.txt
 
