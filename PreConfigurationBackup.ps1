aa46736455b868f41dd2e453159ff6d5f9df7275
Eduardo Hamue <eduardo.hamue@aspentech.com>
2024-03-13 22:09:11 +0000

    Adding Backup

---
+++ PreConfigurationBackup.ps1
@@ -0,0 +1,25 @@
+﻿Write-Host "Changing machine name"
+
+Start-Process -FilePath 'C:\Program Files\AspenTech\InfoPlus.21\db21\code\IP21Rename.exe' -ArgumentList "-nq" -WorkingDirectory 'C:\Program Files\AspenTech\InfoPlus.21\db21\code\' -Wait >> C:\p4\ProMVConfiguration.git\logs\logRenameMachine.txt
+
+
+Write-Host "info plus starting"
+& "C:\Program Files\AspenTech\InfoPlus.21\db21\code\tsk_client.exe" /start_infoplus21 >> C:\p4\ProMVConfiguration.git\logs\logStartInfoPlus.txt
+
+
+Write-Host "Virtual Plant starting"
+
+C:\ProgramData\Aspentech\AspenVirtualPlant\runscript.vbs >> C:\p4\ProMVConfiguration.git\logs\logVirtualPlant.txt
+
+
+
+Write-Host "StartVP"
+
+C:\ProgramData\Aspentech\AspenVirtualPlant\startVP.bat >> C:\p4\ProMVConfiguration.git\logs\logStartVp.txt
+
+
+Write-Host "ISS restart"
+
+iisreset >> C:\p4\ProMVConfiguration.git\logs\logIssReset.txt
+
+iisreset >> C:\p4\ProMVConfiguration.git\logs\logIssReset.txt
