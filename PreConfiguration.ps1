20193c8e59e83d699b7a02016aea16ffc310a4c8
Hamuee <88350059+Hamuee@users.noreply.github.com>
2024-05-14 11:25:45 -0500

    Update PreConfiguration.ps1

--- PreConfiguration.ps1
+++ PreConfiguration.ps1
@@ -26,5 +26,4 @@ iisreset >> C:\p4\ProMVConfiguration.git\logs\logIssReset.txt
 
 
 Write-Host "Update Node JS"
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) >> C:\p4\ProMVConfiguration.git\logs\installChocolate.txt
 choco install nodejs >> C:\p4\ProMVConfiguration.git\logs\installNodeJS.txt
